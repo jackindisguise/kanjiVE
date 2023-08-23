d939b6f704a7fc004b07a669977f3d49f08c7e8f
nowotato <incognitus.me@gmail.com>
2023-08-23 09:57:40 -0500

    chore: added some comments

--- kanjive.js
+++ kanjive.js
@@ -4,20 +4,33 @@ const sub = require("./sub.json");
 const outJSON = "kanjive.json";
 const outTXT = "kanjive.txt";
 
+// use separately compiled data to add new visual elements to kanji
+const added = 0;
 for(let kanji of kanjivg){
+	// for sorting purposes, create new elements array
+	let newElements = [];
+
+	// check if this kanji has custom constituent parts
 	for(let element of sub){
 		if(element.element === kanji){
-			kanji.elements = kanji.elements.concat(element.parts);
+			newElements = newElements.concat(element.parts);
+			added += element.parts.length;
 		}
 	}
 
+	// check each element for custom constituent parts
 	for(let element of kanji.elements){
+		newElements.push(element);
 		for(let _element of sub){
 			if(_element.element === element){
-				kanji.elements = kanji.elements.concat(_element.parts);
+				newElements = newElements.concat(_element.parts);
+				added += element.parts.length;
 			}
 		}
 	}
+
+	// replace
+	kanji.elements = newElements;
 }
 
 let lines = [];
